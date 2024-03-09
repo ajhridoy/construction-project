d67450dde54c328ec0ba51ea63ec9f9cd38a8a3e
ajhridoy <ajhridoy461@gmail.com>
2024-03-09 22:11:15 +0600

    responsive added

--- swipper.js
+++ swipper.js
@@ -17,12 +17,26 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 5,
+    slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
+    },
+    breakpoints: {
+      577: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 30,
+      },
+      // 1230: {
+      //   slidesPerView: 5,
+      //   spaceBetween: 50,
+      // },
     },
     pagination: {
       el: ".swiper-pagination2",
@@ -31,12 +45,26 @@ var swiper = new Swiper(".mySwiper", {
   });
 
   var swiper = new Swiper(".mySwiper3", {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     autoplay: {
       delay: 4000,
       disableOnInteraction: false,
+    },
+    breakpoints: {
+      577: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      993: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      // 1024: {
+      //   slidesPerView: 5,
+      //   spaceBetween: 50,
+      // },
     },
     pagination: {
       el: ".swiper-pagination2",
